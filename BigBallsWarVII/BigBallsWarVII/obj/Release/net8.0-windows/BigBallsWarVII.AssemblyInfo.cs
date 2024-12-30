cc4f997084d644b30ce76d7d70fa28a209027fdc
TomoChen <e1104421057@gm.lhu.edu.tw>
2024-12-31 06:37:41 +0800

    更改一些東西

--- BigBallsWarVII/BigBallsWarVII/obj/Release/net8.0-windows/BigBallsWarVII.AssemblyInfo.cs
+++ BigBallsWarVII/BigBallsWarVII/obj/Release/net8.0-windows/BigBallsWarVII.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("BigBallsWarVII")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1537670fe3572709cd6fa3463890ac052e82904b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BigBallsWarVII")]
